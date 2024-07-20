ecfa43149535ec5a7f8eec15bbf45940e95b59c6
Hygor Martins <hygor.martins74@gmail.com>
2024-07-20 11:25:08 -0300

    feat: add project with initial routes

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+
+import { confirmParticipant } from "./routes/confirm-participant";
+import { confirmTrip } from "./routes/confirm-trips";
+import cors from "@fastify/cors";
+import { createActivity } from "./routes/create-activity";
+import { createLink } from "./routes/create-link";
+import { createTrip } from "./routes/create-trip";
+import fastify from "fastify";
+import { getActivities } from "./routes/get-activities";
+import { getLinks } from "./routes/get-links";
+
+const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Server is running on port 3333");
+});
